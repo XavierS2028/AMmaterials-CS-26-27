70aaaf85e80c7f88984fde70c0acc99246950e9d
XavierS2028 <xavier.schrage@stu.evsck12.com>
2026-09-18 09:40:14 -0500

    Added some basic Python notes.

---
+++ PythonNotes/Notes/HelloInput.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+
+print("Hello " + name + "!")
+
+for i in range(0, 10):
+    print(i)
+
+#This is a single-line comment
+
+'''
+This is a multi-line comment
+'''
+
+print("Hello, " + input("What is your name again?") + "!")
